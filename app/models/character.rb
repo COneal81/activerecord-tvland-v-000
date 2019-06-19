bbc82ff563367f5fcaf799bda9aef2d654be682c
Kari O'Neal <triadfirewood@gmail.com>
2019-06-19 21:23:26 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,10 +1,12 @@
+require 'pry'
+
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
   belongs_to :network
   
   def say_that_thing_you_say
-    
+    binding.pry 
   end 
   
 end
